860dbb2f3d3cfcb2f259cf774543d5ffa88fb122
ncsn <nagy.2000.csaba@gmail.com>
2022-03-25 17:40:49 +0100

    +bővítés

---
+++ src/main/java/hu/unideb/inf/FXMLUserSiteSceneController.java
@@ -0,0 +1,35 @@
+package hu.unideb.inf;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FXMLUserSiteSceneController {
+    @FXML
+    private Button buttonMenu;
+
+    @FXML
+    private Button buttonKosar;
+
+    @FXML
+    private Button buttonKapcsolat;
+
+    @FXML
+    void menuPushed(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void kosarPushed(ActionEvent event) throws IOException {
+
+    }
+    @FXML
+    void kapcsolatPushed(ActionEvent event) throws IOException {
+
+    }
+}
