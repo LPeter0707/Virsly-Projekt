84ca40a1380423ab35f82bc245f357ba46315895
racznandor <racznandor2@gmail.com>
2022-03-26 14:49:58 +0100

    Menu_bővítés

--- src/main/java/hu/unideb/inf/FXMLScenes.java
+++ src/main/java/hu/unideb/inf/FXMLScenes.java
@@ -11,12 +11,12 @@ public class FXMLScenes {
     public Pane getPage(String fileName){
         try {
             URL fileUrl = FXMLUserSiteSceneController.class.getResource("/fxml/" + fileName + ".fxml");
-            System.out.println(fileUrl);
             if (fileUrl == null){
                 throw new java.io.FileNotFoundException("Nem tudja megnyitni a fájlt");
             }
-
-            view = new FXMLLoader().load(fileUrl);
+            else{
+                view = new FXMLLoader().load(fileUrl);
+            }
 
         }
         catch (Exception e)
--- src/main/java/hu/unideb/inf/FXMLUserSiteSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLUserSiteSceneController.java
@@ -3,41 +3,38 @@ package hu.unideb.inf;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class FXMLUserSiteSceneController {
-    @FXML
-    private Button buttonMenu;
 
     @FXML
-    private Button buttonKosar;
-
-    @FXML
-    private Button buttonKapcsolat;
-
-    @FXML
-    private BorderPane mainPane;
+    public AnchorPane mainPane;
 
     @FXML
     void menuPushed(ActionEvent event) throws IOException {
         FXMLScenes object = new FXMLScenes();
         Pane view = object.getPage("FXMLMenu");
-        mainPane.setCenter(view);
+        mainPane.getChildren().setAll(view);
     }
 
     @FXML
     void kosarPushed(ActionEvent event) throws IOException {
         FXMLScenes object = new FXMLScenes();
-        Pane view = object.getPage("FXMLproba");
-        mainPane.setCenter(view);
+        Pane view = object.getPage("FXMLKosar");
+        mainPane.getChildren().setAll(view);
     }
     @FXML
     void kapcsolatPushed(ActionEvent event) throws IOException {
         FXMLScenes object = new FXMLScenes();
         Pane view = object.getPage("FXMLKapcsolat");
-        mainPane.setCenter(view);
+        mainPane.getChildren().setAll(view);
     }
 }
