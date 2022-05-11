864e447e6057e0fecb517e31e72667368aa1d6bb
szabosandor0910 <78804107+szabosandor0910@users.noreply.github.com>
2022-05-11 14:03:55 +0200

    Order_kezdes

--- src/main/java/hu/unideb/inf/FXMLOrderSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLOrderSceneController.java
@@ -1,5 +1,8 @@
 package hu.unideb.inf;
 
+import hu.unideb.inf.model.JpaStorageDAO;
+import hu.unideb.inf.model.Storage;
+import hu.unideb.inf.model.StorageDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,14 +10,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.awt.*;
 import java.io.IOException;
+import java.util.List;
 
 public class FXMLOrderSceneController {
+
+    private ObservableList<Integer> valasztasok = FXCollections.observableArrayList(0,1,2,3,4,5);
+
+    @FXML
+    private ChoiceBox<Integer> nyhdp;
+
     @FXML
     private Button buttonBack;
 
     @FXML
+    private Button addElement;
+
+    @FXML
     private Button buttonNext;
+
+    @FXML
+    private TextArea OrderScene;
+    public static TextArea OrderScene_static;
 
     @FXML
     void backPushed(ActionEvent event) throws IOException {
@@ -41,4 +61,26 @@ public class FXMLOrderSceneController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void initialize(){
+        OrderScene_static = OrderScene;
+        nyhdp.setValue(0);
+        nyhdp.setItems(valasztasok);
+
+        AlapanyagKiir();
+    }
+
+    static void AlapanyagKiir(){
+        StorageDao sdao = new JpaStorageDAO();
+        List<Storage> alapanyagok = sdao.getStorage();
+
+    }
+
+    @FXML
+    void addElementPushed(ActionEvent event) throws IOException {
+
+    }
+
+
 }
