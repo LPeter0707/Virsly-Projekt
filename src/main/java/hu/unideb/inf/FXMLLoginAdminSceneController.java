7cc7e5e7ec48b0a9ffcb8929424506d2f24d9d16
ncsn <nagy.2000.csaba@gmail.com>
2022-05-11 20:43:14 +0200

    Kerelem

--- src/main/java/hu/unideb/inf/FXMLLoginAdminSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLLoginAdminSceneController.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.util.Optional;
 
 public class FXMLLoginAdminSceneController {
 
@@ -17,6 +19,9 @@ public class FXMLLoginAdminSceneController {
 
     @FXML
     private Button buttonLogin;
+
+    @FXML
+    private Button buttonKerelem;
 
     @FXML
     void backPushed(ActionEvent event) throws IOException {
@@ -46,5 +51,25 @@ public class FXMLLoginAdminSceneController {
         stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    void kerelemPushed(ActionEvent event) {
+        TextInputDialog textInput = new TextInputDialog();
+        textInput.setTitle("Kérelem");
+        textInput.setHeaderText("Kérelem küldéséhez, adja meg a felhasználónevét");
+        textInput.getDialogPane().setContentText("Felhasználónév:");
+        Optional<String> result = textInput.showAndWait();
+        TextField input = textInput.getEditor();
+
+        if(input.getText() != null && input.getText().toString().length() != 0)
+        {
+            System.out.println("Bemeneti: " + input.getText().toString().length());
+        }
+        else
+        {
+            System.out.println("Nem írt be semmit");
+        }
+
     }
 }
